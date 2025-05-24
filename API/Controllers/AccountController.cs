4153cac1270de6dad3f360ca3647e37696dccf67
Gytis Norutis <gytis.norutis@outlook.com>
2025-05-24 13:05:57 +0100

    section 4

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,4 +1,3 @@
-
 using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography;
 using API.Data;
@@ -9,10 +8,10 @@ using API.DTOs;
 
 namespace API.Controllers;
 
-public class AccountController(DataContext context) : BaseApiController
+public class AccountController(DataContext context, ITokenService tokenService) : BaseApiController
 {
     [HttpPost("register")] //account/register
-    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
     {
         if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
 
@@ -27,11 +26,15 @@ public class AccountController(DataContext context) : BaseApiController
 
         context.Users.Add(user);
         await context.SaveChangesAsync();
-        return user;
+        return new UserDto
+        {
+            Username = user.UserName,
+            Token = tokenService.CreateToken(user)
+        };
     }
 
     [HttpPost("login")] //account/login
-    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
     {
         var user = await context.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
 
@@ -49,7 +52,11 @@ public class AccountController(DataContext context) : BaseApiController
             }
         }
 
-        return user;
+        return new UserDto
+        {
+            Username = user.UserName,
+            Token = tokenService.CreateToken(user)
+        };
     }
 
     
