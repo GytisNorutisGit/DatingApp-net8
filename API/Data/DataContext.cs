db1d6b3d0c67abeab876076ba798203e8afd03cb
Gytis Norutis <gytis.norutis@outlook.com>
2025-06-09 17:31:31 +0100

    setting up the backend for messaging feature

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -1,12 +1,13 @@
-using API.Entities;
+﻿using API.Entities;
 using Microsoft.EntityFrameworkCore;
+
 namespace API.Data;
 
 public class DataContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<AppUser> Users { get; set; }
-
     public DbSet<UserLike> Likes { get; set; }
+    public DbSet<Message> Messages { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
@@ -26,12 +27,15 @@ public class DataContext(DbContextOptions options) : DbContext(options)
             .WithMany(l => l.LikedByUsers)
             .HasForeignKey(s => s.TargetUserId)
             .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Entity<Message>()
+            .HasOne(x => x.Recipient)
+            .WithMany(x => x.MessagesReceived)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.Entity<Message>()
+            .HasOne(x => x.Sender)
+            .WithMany(x => x.MessagesSent)
+            .OnDelete(DeleteBehavior.Restrict);
     }
-
-
 }
-    
-
-
-
-
