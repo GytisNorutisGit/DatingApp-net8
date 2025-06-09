487a494412ca0bf7a0ede4f9dde209c4650ffa95
Gytis Norutis <gytis.norutis@outlook.com>
2025-06-09 17:59:22 +0100

    messaging feature

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -6,10 +6,11 @@ import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { authGuard } from './_guards/auth.guard';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
-import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { preventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
+import { memberDetailedResolver } from './_resolvers/member-detailed.resolver';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -19,14 +20,20 @@ export const routes: Routes = [
         canActivate: [authGuard],
         children: [
             { path: 'members', component: MemberListComponent },
-            { path: 'members/:username', component: MemberDetailComponent },
-            { path: 'member/edit', component: MemberEditComponent, canDeactivate: [preventUnsavedChangesGuard] },
+            {
+                path: 'members/:username', component: MemberDetailComponent,
+                resolve: { member: memberDetailedResolver }
+            },
+            {
+                path: 'member/edit', component: MemberEditComponent,
+                canDeactivate: [preventUnsavedChangesGuard]
+            },
             { path: 'lists', component: ListsComponent },
             { path: 'messages', component: MessagesComponent },
         ]
     },
-    {path: 'errors', component: TestErrorsComponent},
-    {path: 'not-found', component: NotFoundComponent},
-    {path: 'server-error', component: ServerErrorComponent},
-    { path: '**', component: HomeComponent, pathMatch: 'full' }
+    { path: 'errors', component: TestErrorsComponent },
+    { path: 'not-found', component: NotFoundComponent },
+    { path: 'server-error', component: ServerErrorComponent },
+    { path: '**', component: HomeComponent, pathMatch: 'full' },
 ];
